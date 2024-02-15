861fa32fab90f3e5fcdf6b57371ca122a7be3358
Rushikesh Deore <deore.r@northeastern.edu>
2024-02-14 21:46:55 -0500

    Correcting test cases

--- index.js
+++ index.js
@@ -2,19 +2,9 @@ const app = require('./src/app');
 const sequelize = require('./src/config/sequelize');
 const PORT = 3000;
 
-const startServer = async () => {
-  try {
-    // Synchronize the database schema with the defined models
-    await sequelize.sync({ alter: true });
 
-    // Start the server only after the synchronization is successful
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Error synchronizing database:', error);
-  }
-};
-
-startServer();
+// Start the server only after the synchronization is successful
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
--- src/auth.js
+++
@@ -1,4 +0,0 @@
-const generateAuthToken = (user) => {
-  const token = Buffer.from(`${user.email}:${user.password}`).toString('base64');
-  return token;
-};
