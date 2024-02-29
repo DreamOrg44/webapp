84ad8c5b26c591cf4e291a77ba620aec4315584b
Rushikesh Deore <deore.r@northeastern.edu>
2024-02-28 22:56:41 -0500

    Removing database configuration from custom image as db is being created as cloud instance in the form of TF infrastructure

--- packer/scripts/setup-dependencies.sh
+++ packer/scripts/setup-dependencies.sh
@@ -3,28 +3,28 @@
 # Update the system
 sudo yum update -y
 
-# Install PostgreSQL
-sudo yum install -y postgresql-server
+# # Install PostgreSQL
+# sudo yum install -y postgresql-server
 
-# Initialize the PostgreSQL database
-#sudo postgresql-setup initdb
+# # Initialize the PostgreSQL database
+# #sudo postgresql-setup initdb
 
-# Configure PostgreSQL with trust authentication (for demo purposes, adjust as needed)
-sudo PGSETUP_INITDB_OPTIONS=" --auth=trust" postgresql-setup --initdb --unit postgresql --debug
+# # Configure PostgreSQL with trust authentication (for demo purposes, adjust as needed)
+# sudo PGSETUP_INITDB_OPTIONS=" --auth=trust" postgresql-setup --initdb --unit postgresql --debug
 
-# Start and enable PostgreSQL
-sudo systemctl start postgresql
-sudo systemctl enable --now postgresql
+# # Start and enable PostgreSQL
+# sudo systemctl start postgresql
+# sudo systemctl enable --now postgresql
 
-echo 'Setting up PostgreSQL secure installation...'
+# echo 'Setting up PostgreSQL secure installation...'
 
-# Change the password for the postgres user
-#echo "ALTER USER postgres WITH PASSWORD 'root';" | sudo -u postgres psql
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'root';"
-sudo -u postgres psql -c "CREATE DATABASE health_check_db;"
+# # Change the password for the postgres user
+# #echo "ALTER USER postgres WITH PASSWORD 'root';" | sudo -u postgres psql
+# sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'root';"
+# sudo -u postgres psql -c "CREATE DATABASE health_check_db;"
 
 
-echo "PostgreSQL setup completed successfully, and the installed version is $(postgres --version)"
+# echo "PostgreSQL setup completed successfully, and the installed version is $(postgres --version)"
 
 echo "installing node"
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
