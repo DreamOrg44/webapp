f8e16f4e3022bd6719df33c29a442e7d0bfa4482
Rushikesh Deore <deore.r@northeastern.edu>
2024-03-20 20:33:21 -0400

    Changing log location to /var dir and giving appropriate permissions

--- packer/scripts/install_ops_agent.sh
+++ packer/scripts/install_ops_agent.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-
-# Install Ops Agent on CentOS-based system
+echo "Installing google cloud ops_agent"
+cd /tmp/
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-# sudo yum update -y && sudo yum install -y stackdriver-agent
 
-# # Start Ops Agent service
-# sudo systemctl start stackdriver-agent
+sudo tee -a /etc/google-cloud-ops-agent/config.yaml << EOF
+logging:
+  receivers:
+    webapp_log:
+      type: files
+      include_paths:
+        - /var/log/csye6225/webapp.log
+  service:
+    pipelines:
+      webapp_pipeline:
+        receivers:
+          - webapp_log
+EOF
