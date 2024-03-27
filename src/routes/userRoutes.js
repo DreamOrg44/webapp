4df2d1cae0ddd494af5f0b277c50d491330a8089
Rushikesh Deore <deore.r@northeastern.edu>
2024-03-27 16:19:36 -0400

    Publish subscriber changes with email verification and tracking

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,6 +9,7 @@ const authHandler = require('../utils/authHandler');
 router.post('/v1/user', userController.createUser);
 router.put('/v1/user/self', authHandler, userController.updateUser);
 router.get('/v1/user/self', authHandler, userController.getUserInfo);
+router.get('/verify', userController.updateUserEmailVerification);
 //added to check if user is authenticated, will have to see the need before making it a final appraoch
 
 module.exports = router;
