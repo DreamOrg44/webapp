e85f41b5d350ba043f2ad6b9f84d83230a710f44
Rushikesh Deore <deore.r@northeastern.edu>
2024-04-16 16:34:27 -0400

    v2 changes endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,9 +6,9 @@ const userController = require('../controller/userController');
 const authHandler = require('../utils/authHandler');
 
 // User Operations
-router.post('/v1/user', userController.createUser);
-router.put('/v1/user/self', authHandler, userController.updateUser);
-router.get('/v1/user/self', authHandler, userController.getUserInfo);
+router.post('/v2/user', userController.createUser);
+router.put('/v2/user/self', authHandler, userController.updateUser);
+router.get('/v2/user/self', authHandler, userController.getUserInfo);
 router.get('/verify', userController.updateUserEmailVerification);
 //added to check if user is authenticated, will have to see the need before making it a final appraoch
 
